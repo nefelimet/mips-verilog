6ec2af7ac159d1688d551f12f0f7bda97859fd06
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-01-17 19:45:13 +0200

    Update ALUSTAGE.v

--- ALUSTAGE.v
+++ ALUSTAGE.v
@@ -23,17 +23,24 @@ module ALUSTAGE(
     input [31:0] RF_B,
     input [31:0] Immed,
     input ALU_Bin_sel,
+	 input [31:0] lui_out,
+	 input lui,
     input [3:0] ALU_func,
-    output [31:0] ALU_out
+    output [31:0] ALU_out,
+	 output Zero
     );
 	 
 	 wire [31:0] ALU_in_B;
+	 wire [31:0] lui_mux_out;
+	 
+	 //Create MUX for lui
+	 mux2to1 lui_mux (.D1(Immed), .D2(lui_out), .Sel(lui), .Dout(lui_mux_out));
 	 
 	 //Create MUX
-	 mux2to1 mux_alu (.D1(RF_B), .D2(Immed), .Sel(ALU_Bin_sel), .Dout(ALU_in_B));
+	 mux2to1 mux_alu (.D1(RF_B), .D2(lui_mux_out), .Sel(ALU_Bin_sel), .Dout(ALU_in_B));
 	 
 	 //Create ALU
-	 alu alu_stage (.A(RF_A), .B(ALU_in_B), .Op(ALU_func), .Out(ALU_out), .Zero());
+	 alu alu_stage (.A(RF_A), .B(ALU_in_B), .Op(ALU_func), .Out(ALU_out), .Zero(Zero));
 
 
 endmodule
