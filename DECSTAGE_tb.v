529c01a4d82438773e1b8bb4cb84a144151db3a5
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-01-19 02:13:03 +0200

    Fix extended testbench and add better tests

--- DECSTAGE_tb.v
+++ DECSTAGE_tb.v
@@ -32,16 +32,14 @@ module DECSTAGE_tb;
 	reg RF_WrData_sel;
 	reg RF_B_sel;
 	reg Clk = 0;
-	reg lb;
 	reg sb;
+	reg lb;
 
 	// Outputs
 	wire [31:0] Immed;
 	wire [31:0] RF_A;
-	wire [31:0] RF_B;
+	wire [31:0] RF_B_or_sb;
 	wire [31:0] lui_out;
-	wire [31:0] lb_out;
-	wire [31:0] sb_out;
 
 	// Instantiate the Unit Under Test (UUT)
 	DECSTAGE uut (
@@ -51,10 +49,12 @@ module DECSTAGE_tb;
 		.MEM_out(MEM_out), 
 		.RF_WrData_sel(RF_WrData_sel), 
 		.RF_B_sel(RF_B_sel), 
-		.Clk(Clk), 
+		.Clk(Clk),
+		.sb(sb),
+		.lb(lb),
 		.Immed(Immed), 
 		.RF_A(RF_A), 
-		.RF_B(RF_B),
+		.RF_B_or_sb(RF_B_or_sb),
 		.lui_out(lui_out)
 	);
 	
@@ -62,41 +62,85 @@ module DECSTAGE_tb;
 
 	initial begin
 	
-		//Write value '7' to $r1
-		Instr = 32'b11100000000000010000100000000111;
+		//#li 7 $r1
+		Instr = 32'b11100000000000010000000000000111;
 		RF_WrEn = 1;
 		ALU_out = 32'b00000000000000000000000000000111;
 		MEM_out = 1;
 		RF_WrData_sel = 0;
-		RF_B_sel = 0;
+		RF_B_sel = 1;
+		sb = 0;
+		lb = 0;
 		#10;
 		
-		//Write value '5' to $r2
-		Instr = 32'b11100000000000100000100000000101;
+		//#li 5 $r2
+		Instr = 32'b11100000000000100000000000000101;
 		RF_WrEn = 1;
 		ALU_out = 32'b00000000000000000000000000000101;
 		MEM_out = 1;
 		RF_WrData_sel = 0;
+		RF_B_sel = 1;
+		sb = 0;
+		lb = 0;
 		#10;
 		
-		//Write value '12' to $r3
-		ALU_out = 32'b00000000000000000000000000001100;
-		RF_WrEn = 1;
-		Instr = 32'b11100000000000110000100000000101;
-		#10;
-		
-		//Read $r3
-		RF_WrEn = 0;
-		Instr = 32'b00000000011000000000100000000000;
-		#10;
-		
-		// add $r1 $r2 $r3
+		//#add $r1 $r2 $r3
 		Instr = 32'b10000000001000110001000000110000;
+		RF_WrEn = 1;
+		ALU_out = 32'b00000000000000001100000000001111;
+		MEM_out = 1;
+		RF_WrData_sel = 0;
+		RF_B_sel = 0;
+		sb = 0;
+		lb = 0;
 		#10;
 		
+		//#sb 
+		Instr = 32'b00011100001000110000000000000101;
+		RF_WrEn = 0;
+		ALU_out = 0;
+		MEM_out = 0;
+		RF_WrData_sel = 0;
+		RF_B_sel = 1;
+		sb = 1;
+		lb = 0;
+		#10;
+		//$display("RF_B: %b, sb_out: %b", uut.RF_B, uut.sb_out);
 		
+		//#lb
+		Instr = 32'b00001100001001000000000000000101;
+		RF_WrEn = 1;
+		ALU_out = 0;
+		MEM_out = 32'b11001111011001000111100001010101;
+		RF_WrData_sel = 1;
+		RF_B_sel = 1;
+		sb = 0;
+		lb = 1;
+		#10;
 		
-
+		//#lui
+		Instr = 32'b11100100000001011101100000000111;
+		RF_WrEn = 1;
+		ALU_out = 32'b11011000000001110000000000000000;
+		MEM_out = 0;
+		RF_WrData_sel = 0;
+		RF_B_sel = 1;
+		sb = 0;
+		lb = 0;
+		#10;
+		
+		//beq $r1 $r2 10
+		Instr = 32'b00000000001000100000000000001010;
+		RF_WrEn = 0;
+		ALU_out = 0;
+		MEM_out = 0;
+		RF_WrData_sel = 0;
+		RF_B_sel = 1;
+		sb = 0;
+		lb = 0;
+		#10;
+		
+	//Simulation time: 70ns
 	end
       
 endmodule
