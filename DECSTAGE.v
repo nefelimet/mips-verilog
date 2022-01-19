22274e03292d0165e75136e299e5b52254253b30
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-01-19 02:14:29 +0200

    Fix extended DECSTAGE
    1. MEM_out needed to be input, not MEM_out_or_lb
    2. Threw away lui_unit and get_byte, since their functions could easily be done through code
    3. mux2 had a small mistake

--- DECSTAGE.v
+++ DECSTAGE.v
@@ -22,7 +22,7 @@ module DECSTAGE(
     input [31:0] Instr,
     input RF_WrEn,
     input [31:0] ALU_out,
-	 input [31:0] MEM_out_or_lb,
+	 input [31:0] MEM_out,
     input RF_WrData_sel,
     input RF_B_sel,
     input Clk,
@@ -42,26 +42,32 @@ module DECSTAGE(
 	wire [31:0] write_data;
 	wire [31:0] lb_out;
 	wire [31:0] sb_out;
-	wire [31:0] MEM_out;
+	wire [31:0] MEM_out_or_lb;
 	wire [31:0] RF_B;
 	
 	//Create output Immed
-	wire [31:0] temp = {16'b0000000000000000, instr4};
-	assign Immed = temp;
+	wire [31:0] temp1 = {16'b0000000000000000, instr4};
+	assign Immed = temp1;
 	
 	//Create lui unit
-	lui_unit lui (.immed(instr4), .lui_out(lui_out));
+	//lui_unit lui (.immed(instr4), .lui_out(lui_out));
+	wire [31:0] temp2 = {instr4, 16'b0000000000000000};
+	assign lui_out = temp2;
 	
 	//Create MUXs
 	mux2to1_5bit mux1 (.D1(instr2), .D2(instr3), .Sel(RF_B_sel), .Dout(read_reg2));
-	mux2to1 mux2 (.D1(ALU_out), .D2(MEM_out), .Sel(RF_WrData_sel), .Dout(write_data));
+	mux2to1 mux2 (.D1(ALU_out), .D2(MEM_out_or_lb), .Sel(RF_WrData_sel), .Dout(write_data));
 	
 	//Create register file
 	reg_file RF (.Ard1(instr1), .Ard2(read_reg2), .Awr(instr3), .Dout1(RF_A), .Dout2(RF_B), .Din(write_data), .WrEn(RF_WrEn), .Clk(Clk));
 
 	//Create get_byte units for lb and sb instructions
-	get_byte lb_byte (.Din(RF_B), .Dout(lb_out));
-	get_byte sb_byte (.Din(MEM_out), .Dout(sb_out));
+	//get_byte lb_byte (.Din(MEM_out), .Dout(lb_out));
+	wire [31:0] temp3 = {24'b000000000000000000000000, MEM_out[7:0]};
+	assign lb_out = temp3;
+	//get_byte sb_byte (.Din(RF_B), .Dout(sb_out));
+	wire [31:0] temp4 = {24'b000000000000000000000000, RF_B[7:0]};
+	assign sb_out = temp4;
 	
 	mux2to1 mux_lb (.D1(MEM_out), .D2(lb_out), .Sel(lb), .Dout(MEM_out_or_lb));
 	mux2to1 mux_sb (.D1(RF_B), .D2(sb_out), .Sel(sb), .Dout(RF_B_or_sb));
